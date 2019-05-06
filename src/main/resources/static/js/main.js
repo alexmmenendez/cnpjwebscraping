2550c0ae299f8488e178485c2d05bc8e0781c265
alexmenendezdocket <alex.menendez@docket.com.br>
2019-05-06 13:11:36 -0300

    mascara de cpf e cnpj

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -13,49 +13,140 @@ $(document).ready(function () {
 
 });
 
-    function copyToClipboard(element) {
-        var $temp = $("<input>");
-        $("body").append($temp);
-        $temp.val($(element).text()).select();
-        document.execCommand("copy");
-        $temp.remove();
+function mascaraMutuario(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout('execmascara()',1)
+}
+
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+
+function cpfCnpj(v){
+
+    //Remove tudo o que não é dígito
+    v=v.replace(/\D/g,"")
+
+    if (v.length < 14) { //CPF
+
+        //Coloca um ponto entre o terceiro e o quarto dígitos
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")
+
+        //Coloca um ponto entre o terceiro e o quarto dígitos
+        //de novo (para o segundo bloco de números)
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")
+
+        //Coloca um hífen entre o terceiro e o quarto dígitos
+        v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+
+    } else { //CNPJ
+
+        //Coloca ponto entre o segundo e o terceiro dígitos
+        v=v.replace(/^(\d{2})(\d)/,"$1.$2")
+
+        //Coloca ponto entre o quinto e o sexto dígitos
+        v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
+
+        //Coloca uma barra entre o oitavo e o nono dígitos
+        v=v.replace(/\.(\d{3})(\d)/,".$1/$2")
+
+        //Coloca um hífen depois do bloco de quatro dígitos
+        v=v.replace(/(\d{4})(\d)/,"$1-$2")
+
     }
 
-    function fire_ajax_submit() {
+    return v
+}
 
-        $("#nomeRazaoSocial").addClass('d-none');
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
 
-        var cpfcnpj = $("#cpfcnpj").val();
+function fire_ajax_submit() {
 
-        $("#btn-consultar").prop("disabled", true);
+    $("#nomeRazaoSocial").addClass('d-none');
 
-        $.ajax({
-            type: "POST",
-            contentType: "application/json",
-            url: "/api/v1/cnpjwebscraping/consulta/cpf-cnpj",
-            data: JSON.stringify(cpfcnpj),
-            dataType: 'json',
-            cache: false,
-            timeout: 600000,
-            success: function (data) {
+    var cpfcnpj = $("#cpfcnpj").val();
 
-                var nomeRazaoSocial = data.nome;
+    $("#btn-consultar").prop("disabled", true);
 
-                 $("#nomeRazaoSocial span").text(nomeRazaoSocial);
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "/api/v1/cnpjwebscraping/consulta/cpf-cnpj",
+        data: JSON.stringify(cpfcnpj),
+        dataType: 'json',
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
 
-                 $("#nomeRazaoSocial").toggleClass('d-none');
+            var nomeRazaoSocial = data.nome;
 
+            $("#nomeRazaoSocial span").text(nomeRazaoSocial);
 
-                console.log("SUCCESS : ", nomeRazaoSocial);
-                $("#btn-consultar").prop("disabled", false);
+            $("#nomeRazaoSocial").toggleClass('d-none');
 
-            },
-            error: function (e) {
+            $("#btn-consultar").prop("disabled", false);
 
-                console.log("ERROR : ", e);
-                $("#btn-consultar").prop("disabled", false);
+            console.log("SUCCESS : ", nomeRazaoSocial);
 
-            }
+        },
+        error: function (e) {
+
+            console.log("ERROR : ", e);
+            $("#btn-consultar").prop("disabled", false);
+
+        }
     });
 
 }
+
+/*function mascaraMutuario(o,f){
+   v_obj=o
+   v_fun=f
+   setTimeout('execmascara()',1)
+}
+
+function execmascara(){
+   v_obj.value=v_fun(v_obj.value)
+}
+
+function cpfCnpj(v){
+
+   //Remove tudo o que não é dígito
+   v=v.replace(/\D/g,"")
+
+   if (v.length <= 14) { //CPF
+
+       //Coloca um ponto entre o terceiro e o quarto dígitos
+       v=v.replace(/(\d{3})(\d)/,"$1.$2")
+
+       //Coloca um ponto entre o terceiro e o quarto dígitos
+       //de novo (para o segundo bloco de números)
+       v=v.replace(/(\d{3})(\d)/,"$1.$2")
+
+       //Coloca um hífen entre o terceiro e o quarto dígitos
+       v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+
+   } else { //CNPJ
+
+       //Coloca ponto entre o segundo e o terceiro dígitos
+       v=v.replace(/^(\d{2})(\d)/,"$1.$2")
+
+       //Coloca ponto entre o quinto e o sexto dígitos
+       v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
+
+       //Coloca uma barra entre o oitavo e o nono dígitos
+       v=v.replace(/\.(\d{3})(\d)/,".$1/$2")
+
+       //Coloca um hífen depois do bloco de quatro dígitos
+       v=v.replace(/(\d{4})(\d)/,"$1-$2")
+
+   }
+
+   return v*/
