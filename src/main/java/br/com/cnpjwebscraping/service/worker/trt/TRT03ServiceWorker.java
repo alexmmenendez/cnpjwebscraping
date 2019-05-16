631c41950867d45653eef606fe2b5d01babd2fb6
alexmenendezdocket <alex.menendez@docket.com.br>
2019-05-16 12:07:34 -0300

    TRT03 consulta de cpf-cnpj implementado

---
+++ src/main/java/br/com/cnpjwebscraping/service/worker/trt/TRT03ServiceWorker.java
@@ -0,0 +1,91 @@
+package br.com.cnpjwebscraping.service.worker.trt;
+
+import br.com.cnpjwebscraping.util.FormatadorString;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class TRT03ServiceWorker implements TRTServiceWorker {
+
+    private static final String URL = "http://as3.trt3.jus.br/certidao/feitosTrabalhistas/aba1.emissao.htm";
+
+    @Override
+    public String consultaNomeCompletoRazaoSocialPeloCPFCNPJ(String cpfCnpj) throws Exception {
+
+        Connection.Response response = Jsoup.connect(URL).execute();
+
+        Map<String, String> cookies = response.cookies();
+
+        Document document = response.parse();
+
+        String viewState = document.select("[name=javax.faces.ViewState]").val();
+
+        cpfCnpj = FormatadorString.removePontuacao(cpfCnpj);
+
+        String inscricao;
+
+        if (cpfCnpj.length() == 14) {
+
+            Jsoup.connect(URL)
+                    .cookies(cookies)
+                    .data("javax.faces.partial.ajax", "true")
+                    .data("javax.faces.source", "form:tipoPessoa")
+                    .data("javax.faces.partial.execute", "form:tipoPessoa")
+                    .data("javax.faces.partial.render", "form:tableDocumentos form:nomeConsulta form:painelNomeInformado form:textoVariacao")
+                    .data("javax.faces.behavior.event", "change")
+                    .data("javax.faces.partial.event", "change")
+                    .data("form", "form")
+                    .data("form:tipoPessoa", "J")
+                    .data("javax.faces.ViewState", viewState)
+                    .execute();
+
+            document = (Jsoup.connect(URL)
+                    .cookies(cookies)
+                    .data("javax.faces.partial.ajax", "true")
+                    .data("javax.faces.source", "form:inputCNPJ")
+                    .data("javax.faces.partial.execute", "form:inputCNPJ")
+                    .data("javax.faces.partial.render", "form:nomeConsulta form:nomeReceitaCPF form:nomeReceitaCNPJ form:botaoConsultar")
+                    .data("javax.faces.behavior.event", "change")
+                    .data("javax.faces.partial.event", "change")
+                    .data("form", "form")
+                    .data("form:tipoPessoa", "J")
+                    .data("form:inputCNPJ", cpfCnpj)
+                    .data("javax.faces.ViewState", viewState)
+                    .execute()).parse();
+
+            inscricao = document.select("update[id=form:nomeReceitaCNPJ]").html();
+
+            inscricao = StringUtils.substringBetween(inscricao, "value=\"", "\"");
+
+            inscricao = inscricao.trim();
+
+        } else {
+            document = Jsoup.connect(URL)
+                    .cookies(cookies)
+                    .data("javax.faces.partial.ajax", "true")
+                    .data("javax.faces.source", "form:inputCPF")
+                    .data("javax.faces.partial.execute", "form:inputCPF")
+                    .data("javax.faces.partial.render", "form:nomeConsulta form:nomeReceitaCPF form:nomeReceitaCNPJ form:botaoConsultar")
+                    .data("javax.faces.behavior.event", "change")
+                    .data("javax.faces.partial.event", "change")
+                    .data("form", "form")
+                    .data("form:tipoPessoa", "F")
+                    .data("form:inputCPF", cpfCnpj)
+                    .data("javax.faces.ViewState", viewState)
+                    .execute().parse();
+
+            inscricao = document.select("update[id=form:nomeReceitaCPF]").html();
+
+            inscricao = StringUtils.substringBetween(inscricao, "value=\"", "\"");
+
+            inscricao = inscricao.trim();
+        }
+
+        return inscricao;
+    }
+}
---
+++ src/test/java/br/com/cnpjwebscraping/worker/trt/TRT02ServiceWorkerTest.java
@@ -0,0 +1,28 @@
+package br.com.cnpjwebscraping.worker.trt;
+
+import br.com.cnpjwebscraping.service.worker.trt.TRT02ServiceWorker;
+import br.com.cnpjwebscraping.worker.ServiceWorkerIntegracaoTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TRT02ServiceWorkerTest extends ServiceWorkerIntegracaoTest {
+
+    @InjectMocks
+    private TRT02ServiceWorker worker;
+
+    @Test
+    public void consultarSintegra() throws Exception {
+
+        String cpfcnpj = "46158810886";
+
+        String nome = worker.consultaNomeCompletoRazaoSocialPeloCPFCNPJ(cpfcnpj);
+
+        Assert.assertEquals(nome, "ALEX MACHADO MENENDEZ");
+
+    }
+
+}
